01dd6900f984ca4dd8639429b32b91e37e82950b
Albdarm <albdarm@dewv.edu>
2018-09-18 04:10:04 +0000

    Updates via sync script

--- projectLoader.js
+++ projectLoader.js
@@ -1,3 +1,5 @@
 import {game} from "./sgc/sgc.js";
 game.preloadImage("ninja.jpg", 48, 48);
 game.preloadImage("ninjaHouse.jpg", 48, 48);
+game.preloadImage("ninjaSpellSheet", 48, 48);
+game.preloadImage("ninjaSheet", 48, 48);
